9540639399f327b79e48624be0a7b3abe8e07acd
dinanli2002 <dinan.li2003@gmail.com>
2024-11-29 15:25:43 +0100

    #2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 android {
     namespace = "com.example.frontendgroup"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.frontendgroup"
