850617fb524062d4fc82bb1c64d9ef143e30b6b2
dinanli2002 <dinan.li2003@gmail.com>
2025-01-24 16:26:15 +0100

    #23

---
+++ app/src/main/java/com/example/frontendgroup/LoginViewModel.kt
@@ -0,0 +1,60 @@
+package com.example.frontendgroup
+
+import android.util.Log
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.frontendgroup.retrofit.NurseInterface
+import com.example.frontendgroup.retrofit.RemoteNurseUiState
+import com.example.frontendgroup.stricturedata.Nurse
+import kotlinx.coroutines.launch
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+class LoginViewModel: ViewModel() {
+    var remoteMessageUiState: RemoteNurseUiState by mutableStateOf(RemoteNurseUiState.Loading)
+    private set
+    private val client = OkHttpClient.Builder()
+        .addInterceptor { chain ->
+            val request = chain.request()
+            Log.d("LoginViewModel", "Request URL: ${request.url}")
+            val response = chain.proceed(request)
+            val headers = response.headers
+            for (name in headers.names()) {
+                Log.d("LoginViewModel", " HEADERS: $name: ${headers[name]}")
+            }
+            Log.d("LoginViewModel", "Response Status Code: ${response.code}")
+            response
+        }.build()
+    private val api: NurseInterface = Retrofit.Builder()
+        .baseUrl("http://10.0.2.2:8080/")
+        .addConverterFactory(GsonConverterFactory.create())
+        .client(client)
+        .build()
+        .create(NurseInterface::class.java)
+    fun login(username: String, password: String) {
+        viewModelScope.launch {
+            remoteMessageUiState = RemoteNurseUiState.Loading
+            try {
+                val response = api.login(username, password)
+                if (response.isSuccessful) {
+                    val message = response.body()?.string() ?: "Unknown error"
+                    Log.d("LoginViewModel", "Login successful: $message")
+                    remoteMessageUiState = RemoteNurseUiState.Success(Nurse("1", username, password))
+                } else {
+                    Log.e("LoginViewModel", "Login failed with code: ${response.code()}")
+                    remoteMessageUiState = RemoteNurseUiState.Error
+                }
+                /*val response = api.login(username, password)
+                Log.d("LoginViewModel", "Login successful: ${response.body()}")
+                remoteMessageUiState = RemoteNurseUiState.Success(Nurse("1", "nurse1", "nurse1"))*/
+            } catch (e: Exception) {
+                Log.e("LoginViewModel", "Login error: ${e.message}", e)
+                remoteMessageUiState = RemoteNurseUiState.Error
+            }
+        }
+    }
+    }
